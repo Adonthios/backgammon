ffce27f715816d72036365d9ffc7e447b54ddaf7
Adonthios <adonthios@gmail.com>
2019-08-24 13:00:02 +0200

    First base

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Sun/>
+  </div>
+</template>
+
+<script>
+import Sun from './components/Sun.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Sun
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Sun.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="sun">
+    <span ><i></i></span>
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+.sun {
+  background: yellow;
+  border: 2px solid orange;
+  width: 100px;
+ height: 100px;
+}
+</style>
